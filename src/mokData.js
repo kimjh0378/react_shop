5718797c8d365f181b7dd37adb0538479c62ef42
kimjh0378 <gwer2523@naver.com>
2025-07-18 16:34:58 +0900

    메인페이지, 컴포넌트 분리, 리액트 라우터 지정

---
+++ src/mokData.js
@@ -0,0 +1,26 @@
+let data = [
+  {
+    id : 0,
+    title:'apple',
+    content: '빨간색 사과',
+    price: 12000
+  },
+  {
+    id : 1,
+    title:'strawberry',
+    content: '빨간색 딸기',
+    price: 25000
+  },
+  {
+    id : 2,
+    title:'watermelon',
+    content: '빨간색 수박',
+    price: 17000
+  }
+
+
+
+
+]
+
+export default data
